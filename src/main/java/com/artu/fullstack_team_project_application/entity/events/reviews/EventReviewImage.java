e2215b941bd3d77eb4a4334857b6ab6023fecba9
이상혁 <hyuck6029@gmail.com>
2025-03-20 15:54:05 +0900

    컬럼 재수정

--- src/main/java/com/artu/fullstack_team_project_application/entity/events/reviews/EventReviewImage.java
+++ src/main/java/com/artu/fullstack_team_project_application/entity/events/reviews/EventReviewImage.java
@@ -21,7 +21,7 @@ public class EventReviewImage {
     @Id
     @Column(name = "image_id", nullable = false)
     private Integer id;
-    @Column(name = "event_id", nullable = false,insertable = false, updatable = false)
+    @Column(name = "event_id", nullable = false)
     private int eventId;
     @Column(name = "user_id", nullable = false, length = 50)
     private String userId;
@@ -33,7 +33,7 @@ public class EventReviewImage {
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "event_id", nullable = false)
+    @JoinColumn(name = "event_id", nullable = false, insertable = false, updatable = false)
     private Event event;
 
     @Column(name = "img_url", nullable = false)
