23cfaee55f36a52aa07912e505fcea05c54e7aa4
KWK520 <wonikim520@naver.com>
2025-03-21 15:17:55 +0900

    이벤트 리스트,상세 구현 완

---
+++ src/main/java/com/artu/fullstack_team_project_application/service/event/EventServiceImp.java
@@ -0,0 +1,20 @@
+package com.artu.fullstack_team_project_application.service.event;
+
+import com.artu.fullstack_team_project_application.entity.events.event.Event;
+import com.artu.fullstack_team_project_application.repository.event.EventRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EventService {
+    private final EventRepository eventRepository;
+
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    public List<Event> getAllEvents() {
+        return eventRepository.findAll();
+    }
+}
