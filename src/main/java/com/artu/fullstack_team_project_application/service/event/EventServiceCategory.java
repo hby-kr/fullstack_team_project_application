12da3ceb7cd7d47ff64b36ea68588978aae5d804
KWK520 <wonikim520@naver.com>
2025-03-25 18:14:28 +0900

    카테고리별 이벤트 리스트 출력 완료

--- src/main/java/com/artu/fullstack_team_project_application/service/event/EventServiceCategory.java
+++
@@ -1,9 +0,0 @@
-package com.artu.fullstack_team_project_application.service.event;
-
-import com.artu.fullstack_team_project_application.entity.events.event.Event;
-
-import java.util.List;
-
-public interface EventServiceCategory {
-    List<Event> getEventsByCategory(int categoryId);
-}
