f8477c3163b23e9f931a79252c88dcdf806925c3
KWK520 <wonikim520@naver.com>
2025-04-02 18:56:51 +0900

    수정

--- src/main/java/com/artu/fullstack_team_project_application/repository/event/EventImagesRepository.java
+++ src/main/java/com/artu/fullstack_team_project_application/repository/event/EventImagesRepository.java
@@ -2,11 +2,10 @@ package com.artu.fullstack_team_project_application.repository.event;
 
 import com.artu.fullstack_team_project_application.entity.events.event.EventImage;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
 public interface EventImagesRepository extends JpaRepository<EventImage, Integer> {
-    List<EventImage> findByEventId(int eventId);
+
 }
