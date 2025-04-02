461d771ca208684009e02f487d2b18b56b009968
han-suh <grnadbleu@naver.com>
2025-04-02 10:56:08 +0900

    오류 수정, dto 생성

--- src/main/java/com/artu/fullstack_team_project_application/controller/PostingController.java
+++ src/main/java/com/artu/fullstack_team_project_application/controller/PostingController.java
@@ -1,5 +1,6 @@
 package com.artu.fullstack_team_project_application.controller;
 
+import com.artu.fullstack_team_project_application.dto.UserPageDto;
 import com.artu.fullstack_team_project_application.entity.postings.Posting;
 import com.artu.fullstack_team_project_application.entity.postings.UserFollow;
 import com.artu.fullstack_team_project_application.entity.users.user.User;
@@ -34,34 +35,12 @@ public class PostingController {
     public String userpage(
             @PathVariable String userId,
             Model model) {
-
-        Optional<User> userOptional = userService.readOne(userId);
-        System.out.println("userOptional:" + userOptional);
-        if (!userOptional.isPresent()) {
+        UserPageDto userPageDto=userService.readUserPage(userId);
+        System.out.println("userPageDto:" + userPageDto);
+        if (userPageDto==null) {
             return "redirect:/artu.do";
         }
-
-        User user = userOptional.get();
-        model.addAttribute("user", user);  // user 객체를 모델에 담기
-        model.addAttribute("userId", userId);  // userId도 모델에 담기
-
-        // followee 수
-        Map<String, Long> countFolloweeMap = userService.getCountFollowee(userId);
-        Long countFolloweeCount = countFolloweeMap.get("countFollowee");
-        model.addAttribute("countFolloweeMap", countFolloweeMap);
-        model.addAttribute("countFolloweeCount", countFolloweeCount);
-
-        // follower 수
-        Map<String, Long> countFollowerMap = userService.getCountFollower(userId);
-        Long countFollowerCount = countFollowerMap.get("countFollower");
-        model.addAttribute("countFollowerMap", countFollowerMap);
-        model.addAttribute("countFollowerCount", countFollowerCount);
-
-        // posting 수
-        Map<String, Long> countPostingMap = userService.getCountPosting(userId);
-        Long countPostingCount = countPostingMap.get("countPosting");
-        model.addAttribute("countPostingMap", countPostingMap);
-        model.addAttribute("countPostingCount", countPostingCount);
+        model.addAttribute("userPage", userPageDto);
 
         // 템플릿에 user, followerCounts, followeeCounts를 전달
         return "posting/userpage";
--- src/main/java/com/artu/fullstack_team_project_application/entity/users/user/User.java
+++ src/main/java/com/artu/fullstack_team_project_application/entity/users/user/User.java
@@ -19,6 +19,7 @@ import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
+import java.io.Serializable;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.util.LinkedHashSet;
@@ -34,7 +35,8 @@ import java.util.UUID;
 @Where(clause = "is_used = true") // 특정 조건을 만족하는 데이터를 조회할 때 추가적인 필터를 적용하는 데 사용
 // is_used = true라는 조건을 추가하여, is_used가 true인 항목만 조회되도록 설정
 @Table(name = "users")
-public class User {
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
 
 
     public enum Gender {M, F}
@@ -72,86 +74,138 @@ public class User {
 //  ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ join 설정 시작
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<UserEventLike> userEventLikes = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<EventReviewImage> eventReviewImages = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<EventReview> eventReviews = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<Event> events = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<Hashtag> hashtags = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "writer")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<Message> messages = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<PasswordChangeHistory> passwordChangeHistories = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<PostingComment> postingComments = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<PostingLike> postingLikes = new LinkedHashSet<>();
 
     // Posting 엔티티와의 관계 설정
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<Posting> postings = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<SearchWord> searchWords = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<UserCart> userCarts = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<UserCoupon> userCoupons = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<UserEnterChatroom> userEnterChatrooms = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<UserEventBmark> userEventBmarks = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<UserImg> userImgs = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<UserInquire> userInquires = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<UserInterest> userInterests = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<UserPoint> userPoints = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<UserPurchaseList> userPurchaseLists = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<UserSetting> userSettings = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<UserloginLog> userloginLogs = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<WidgetDetail> widgetDetails = new LinkedHashSet<>();
 
     @OneToMany(mappedBy = "user")
+    @ToString.Exclude
+    @JsonBackReference
     private Set<Widget> widgets = new LinkedHashSet<>();
 
     // UserFollow 엔티티와의 관계 설정
     @OneToMany(mappedBy = "followers")
     @OrderBy("followedAt ASC") // 정렬
+    @ToString.Exclude
+    @JsonBackReference
     private Set<UserFollow> followers = new LinkedHashSet<>();
 
     // UserFollow 엔티티와의 관계 설정
     @OneToMany(mappedBy = "followees")
     @OrderBy("followedAt ASC") // 정렬
+    @ToString.Exclude
+    @JsonBackReference
     private Set<UserFollow> followees = new LinkedHashSet<>();
 
 }
