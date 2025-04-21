a2fa509cf3e5785e1b2fe6bf3184e8e844be95a2
silverisiron <silverisiron@gmail.com>
2025-04-21 12:49:46 +0900

    ticket purchase

--- src/main/java/com/artu/fullstack_team_project_application/service/event/EventDetailImageService.java
+++
@@ -1,9 +0,0 @@
-package com.artu.fullstack_team_project_application.service.event;
-
-import com.artu.fullstack_team_project_application.entity.events.tickets.EventDetailImage;
-
-import java.util.List;
-
-public interface EventDetailImageService {
-    List<EventDetailImage> getEventDetailImagesByEventId(int eventId);
-}
--- src/main/java/com/artu/fullstack_team_project_application/service/event/EventImagesService.java
+++
@@ -1,9 +0,0 @@
-package com.artu.fullstack_team_project_application.service.event;
-
-import com.artu.fullstack_team_project_application.entity.events.event.EventImage;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-public interface EventImagesService {
-    List<EventImage> getEventImagesByEventId(int eventId);
-}
